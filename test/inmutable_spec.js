c02705c73c558fe94d240fa361f69d8e2ed1ea9a
Danny Viasus(Danjavia) <danny.viasus@comparamejor.com>
2015-12-22 23:10:24 -0500

    trying mocha

--- test/inmutable_spec.js
+++ test/inmutable_spec.js
@@ -1,9 +1,7 @@
 import {expect} from 'chai';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 describe('immutability', () => {
-
-  // ...
 
   describe('A List', () => {
 
@@ -24,6 +22,35 @@ describe('immutability', () => {
         'Trainspotting',
         '28 Days Later'
       ));
+    });
+
+  });
+
+  describe('a tree', () => {
+
+    function addMovie(currentState, movie) {
+      return currentState.update('movies', movies => movies.push(movie));
+    }
+
+    it('is immutable', () => {
+      let state = Map({
+        movies: List.of('Trainspotting', '28 Days Later')
+      });
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later',
+          'Sunshine'
+        )
+      }));
+      expect(state).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later'
+        )
+      }));
     });
 
   });
